a10f4c61512676b51b2d289da329562574c2bbdd
kongchip <myunghwan0321@gmail.com>
2023-06-28 17:56:02 +0900

    중간 작업 PR

---
+++ app/cart/payment/page.tsx
@@ -0,0 +1,7 @@
+export default function payment() {
+  return (
+    <div>
+      <h1>결제페이지 입니다.</h1>
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  let name: string = 'kim';
+  return (
+    <div>
+      <h4 className="title">야채가게</h4>
+      <p className="title-sub">by dev kongchip</p>
+    </div>
+  );
+}
